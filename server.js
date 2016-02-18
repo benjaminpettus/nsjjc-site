24f9b6c9c3e84c1c44a9d920bc32ec447fa1e0b8
benjaminpettus <benjaminpettus@gmail.com>
2016-02-17 16:58:16 -1000

    main routes working

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 var express = require('express');
+
 var app = express();
 
 
+app.get('/', function (req, res) {
+  res.send("Homepage");
+});
 
+app.get('/schedule', function (req, res) {
+  res.send("Schedule");
+});
 
+app.get('/contact', function (req, res) {
+  res.send("Contact");
+});
 
 var server = app.listen(3000, function() {
   console.log('Listening to port', server.address().port);
